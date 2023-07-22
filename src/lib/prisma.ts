750262f6f996f6efbe2e343b6cab3285fcb277c0
luizfernando <luiz-fsilva37@hotmail.com>
2023-07-22 18:25:41 -0300

    Controller de registro

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
-export const prima = new PrismaClient({
+export const prisma = new PrismaClient({
   log: env.NODE_ENV === 'dev' ? ['query'] : [],
 })
