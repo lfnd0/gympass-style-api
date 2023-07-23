d2a006136679ec39458279c279f4a30cfe59c3e3
luizfernando <luiz-fsilva37@hotmail.com>
2023-07-22 21:29:55 -0300

    Lidando com erros do use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '@/use-cases/register'
 import { z } from 'zod'
+import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -17,8 +18,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({ name, email, password })
-  } catch {
-    return reply.status(409).send()
+  } catch (e) {
+    if (e instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: e.message })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
