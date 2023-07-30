eac8ad3e18723b854342744e346ef3080159814a
luizfernando <luiz-fsilva37@hotmail.com>
2023-07-30 19:14:11 -0300

    Caso de uso de check-in

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'nathan@mail.com',
         password: '1001001',
@@ -44,7 +44,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('1001001', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'nathan@mail.com',
         password: '1001002',
