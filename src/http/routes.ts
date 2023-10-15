d54fbf1cd0636624b317b2935fb775f02accc9f0
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-14 22:53:33 -0300

    Criando test environment

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -7,6 +7,5 @@ import { verifyJWT } from './middlewares/verify-jwt'
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
-
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
