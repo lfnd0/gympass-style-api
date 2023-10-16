99948d774fd0d0a967bad748d4b250239798e929
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-16 20:21:36 -0300

    Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
 
-test('ok', () => {})
+describe('Register (2e2)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Logan',
+      email: 'logan@mail.com',
+      password: 'yu789qc',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
