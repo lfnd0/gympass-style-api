e35478f4ee21fa4607f8e9ae8a785db481d7d36e
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-16 20:29:25 -0300

    Teste E2E da autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
 
-test('ok', () => {})
+describe('Authenticate (2e2)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Logan',
+      email: 'logan@mail.com',
+      password: 'yu789qc',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'logan@mail.com',
+      password: 'yu789qc',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
